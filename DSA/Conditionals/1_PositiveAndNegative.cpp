53d8d9272340911e82a90ad83d1a2f2ddc6e24de
Rishav Raj <163753684+rishavkashyap23@users.noreply.github.com>
2024-10-04 20:29:14 +0530

    Add files via upload

---
+++ DSA/Conditionals/1_PositiveAndNegative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    if(n>=0)
+    {
+        cout<<"Number is Postive";
+    }
+    else
+    {
+        cout<<"Number is Negative";
+    }
+    return 0;
+}
