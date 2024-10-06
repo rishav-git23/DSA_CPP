47e951eba7aaab6166ca64449c1be136d610fb50
Rishav Raj <163753684+rishavkashyap23@users.noreply.github.com>
2024-10-07 02:13:07 +0530

    Add files via upload

---
+++ DSA/Conditionals/3_EvenOddNumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number ";
+    cin>>n; 
+    if(n%2==0)
+        cout<<"Even";
+    else
+        cout<<"Odd";
+}
