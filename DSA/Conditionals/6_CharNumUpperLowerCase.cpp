a7ab93db07c841dac88001f89368413948c28791
Rishav Raj <163753684+rishav-git23@users.noreply.github.com>
2024-10-09 00:10:17 +0530

    Add files via upload

---
+++ DSA/Conditionals/6_CharNumUpperLowerCase.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter a char:";
+    cin>>ch;
+    if(ch>=65 && ch<=90){
+        cout<<"upper case\n";
+
+    }
+    else
+        cout<<"lower case\n";
+}
